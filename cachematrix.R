8ce16fca969d1e2573d00ed542d99b8be7c25fcf
ankushpandey1995 <ankushpandey1995@gmail.com>
2020-01-10 10:05:12 +0530

    Updating the Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+# There are a total of two functions which make use of R's lexical scooping rules.
 
-## Write a short comment describing this function
+# makeCacheMatrix initializes a default matrix (if not given as an argument to the function) and then sets the inverse to NULL. 
+#It has functions to set the matrix, get the matrix, set the inverse and get the inverse defined in its own environment. 
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -17,7 +17,10 @@ inv <- NULL
 }
 
 
-## Write a short comment describing this function
+# cacheSolve takes an object of type makeCacheMatrix.
+#x$getmatrix() returns the original matrix either passed previously to makeCacheMatrix / or the $setmatrix function invoked on the list returned by the makeCacheMatrix.
+# After finding the inverse, the setinv() function is called to set the inverse.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
